544d0890efce04b9c2446ac6cfa9003579985fc3
gursewak8427 <gursewaksaggu2043@gmail.com>
2022-07-08 14:53:36 +0530

    fd

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ io.on('connection', (socket) => {
 var port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-  console.log('listening on *:5055');
+  console.log(`listening on ${port}`);
 });
