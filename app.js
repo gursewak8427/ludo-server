23d314a92b7692216123dcd7d80ab31fb2abb7f2
gursewak8427 <gursewaksaggu2043@gmail.com>
2023-03-22 18:50:52 +0530

    ip changed

--- app.js
+++ app.js
@@ -207,6 +207,9 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 5052
-server.listen(PORT, '192.168.134.91', () => {
-    console.log('listening on 192.168.134.91:%d', PORT);
+server.listen(PORT, () => {
+    console.log('listening on ', PORT);
 });
+// server.listen(PORT, '192.168.134.91', () => {
+//     console.log('listening on 192.168.134.91:%d', PORT);
+// });
